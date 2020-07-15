23124b665a9ccf3330e4b3f97d8b7ea570ec1f8d
diazfabio18 <fabiodiaz@est.frba.utn.edu.ar>
2020-07-14 23:32:38 -0300

    json to 26

--- loadTable.js
+++ loadTable.js
@@ -5,12 +5,22 @@ fetch("data.json")
 
 var tbodyw = document.getElementById('tabla-numeros');
 
+/*
 tbodyw.innerHTML += `   <tr>
         <th scope="row">${datos[0].date}</th>
         <td>${datos[0].number}</td>
         <td>${datos[0].place}</td>
         </tr>
-`;
+`;*/
+
+datos.map( function(item,index,array){
+    tbodyw.innerHTML += `<tr>`;
+    tbodyw.innerHTML += `<th scope="row">${item.date}</th>`;
+    tbodyw.innerHTML += `<td>${item.number}</td>`;
+    tbodyw.innerHTML += `<td>${item.place}</td>`;
+    tbodyw.innerHTML += `</tr>`;
+}
+);
 
     })
 //console.log(tbodyw);
