c8c529d151fcf0ebaf55aca7203dd72c1be658ad
diazfabio18 <fabiodiaz@est.frba.utn.edu.ar>
2020-09-06 23:56:26 -0300

    Fixed navbar, footer, and icons on contact

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ $('.nav-link').on('click',function() {
   $('.container').on('click',function() {
     $('.navbar-collapse').collapse('hide');
   });
+
+  particlesJS.load('particles-js', 'particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
